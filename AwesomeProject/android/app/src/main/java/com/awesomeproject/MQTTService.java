bb25da0b5bc1331146ac15e82597b121a2477143
jimmycoconut88 <jimmythenthusiast@gmail.com>
2021-05-09 10:38:39 +0700

    Fix lib importing in MQTTService class

--- AwesomeProject/android/app/src/main/java/com/awesomeproject/MQTTService.java
+++ AwesomeProject/android/app/src/main/java/com/awesomeproject/MQTTService.java
@@ -1,14 +1,14 @@
-import android. content. Context;
-import android. util. Log;
-import org. eclipse. paho. android. service. MqttAndroidClient;
-import org. eclipse. paho. client. mqttv3 .DisconnectedBufferOptions;
-import org. eclipse. paho. client. mqttv3 .IMqttActionListener;
-import org. eclipse. paho. client. mqttv3 .IMqttDeliveryToken;
-import org. eclipse. paho. client. mqttv3 .IMqttToken;
-import org. eclipse. paho. client. mqttv3 .MqttCallbackExtended;
-import org. eclipse. paho. client. mqttv3 .MqttConnectOptions;
-import org. eclipse. paho. client. mqttv3 .MqttException;
-import org. eclipse. paho. client. mqttv3 .MqttMessage;
+import android.content.Context;
+import android.util.Log;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MQTTService {
         final String serverUri ="tcp://io.adafruit.com:1883";
